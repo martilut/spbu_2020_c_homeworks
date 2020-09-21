c3e4630dd501bdbbaac53b69f04a295950760f31
Alena Kropacheva <35008058+martilut@users.noreply.github.com>
2020-09-21 12:47:18 +0300

    Update spiral.c

--- 1.1/spiral.c
+++ 1.1/spiral.c
@@ -11,35 +11,35 @@ int main()
     int b2 = x + 1;
     int arr[n][n];
 
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-      scanf("%d", &arr[i][j]);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &arr[i][j]);
+        }
     }
-  }
 
-  printf("%d ", arr[x][y]);
+    printf("%d ", arr[x][y]);
 
-  for (int k = 0; k < n / 2; k++) {
-    while (x < b2 + k) {
-      x = x + 1;
-      printf("%d ", arr[x][y]);
+    for (int k = 0; k < n / 2; k++) {
+        while (x < b2 + k) {
+            x = x + 1;
+            printf("%d ", arr[x][y]);
+        }
+        while (y < b2 + k) {
+            y = y + 1;
+            printf("%d ", arr[x][y]);
+        }
+        while (x > b1 - k) {
+            x = x - 1;
+            printf("%d ", arr[x][y]);
+        }
+        while (y > b1 - k) {
+            y = y - 1;
+            printf("%d ", arr[x][y]);
+        }
+        while (x < b2 + k) {
+            x = x + 1;
+            printf("%d ", arr[x][y]);
+        }
     }
-    while (y < b2 + k) {
-      y = y + 1;
-      printf("%d ", arr[x][y]);
-    }
-    while (x > b1 - k) {
-      x = x - 1;
-      printf("%d ", arr[x][y]);
-    }
-    while (y > b1 - k) {
-      y = y - 1;
-      printf("%d ", arr[x][y]);
-    }
-    while (x < b2 + k) {
-      x = x + 1;
-      printf("%d ", arr[x][y]);
-    }
-  }
 }
 

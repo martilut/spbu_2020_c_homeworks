7fcaa7e8685da1af569d8bdeb098f2bf33b11c82
Alena Kropacheva <35008058+martilut@users.noreply.github.com>
2020-09-21 12:55:30 +0300

    Update 2moving.c

--- 2moving.c
+++ 2moving.c
@@ -1,30 +1,35 @@
 #include <stdio.h>
 
-int main() {
-
+int main()
+{
     int n, m, i, temp;
     scanf("%d %d", &m, &n);
     int d[m + n];
 
-    for (i = 0; i < m + n; i++){
+    for (i = 0; i < m + n; i++)
+    {
         scanf("%d", &d[i]);
     }
-    for (i = 0; i < m / 2; i++){
+    for (i = 0; i < m / 2; i++)
+    {
         temp = d[i];
         d[i] = d[m - i - 1];
         d[m - i - 1] = temp;
     }
-    for (i = 0; i < n / 2; i++){
+    for (i = 0; i < n / 2; i++)
+    {
         temp = d[i + m];
         d[i + m] = d[m + n - i - 1];
         d[m + n - i - 1] = temp;
     }
-    for (i = 0; i < (n + m) / 2; i++){
+    for (i = 0; i < (n + m) / 2; i++)
+    {
         temp = d[i];
         d[i] = d[n + m - i - 1];
         d[n + m - i - 1] = temp;
     }
-    for (i = 0; i < m + n; i++){
+    for (i = 0; i < m + n; i++)
+    {
         printf("%d ", d[i]);
     }
 }

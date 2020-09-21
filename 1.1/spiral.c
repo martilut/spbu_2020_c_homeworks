ef8e8b51edcd37719806eca46f0f65cb08de871c
Alena Kropacheva <35008058+martilut@users.noreply.github.com>
2020-09-21 12:42:01 +0300

    Update spiral.c

--- 1.1/spiral.c
+++ 1.1/spiral.c
@@ -2,14 +2,14 @@
 
 int main()
 {
-  int n;
-  scanf("%d", &n);
+    int n;
+    scanf("%d", &n);
 
-  int x = n / 2;
-  int y = n / 2;
-  int b1 = x - 1;
-  int b2 = x + 1;
-  int arr[n][n];
+    int x = n / 2;
+    int y = n / 2;
+    int b1 = x - 1;
+    int b2 = x + 1;
+    int arr[n][n];
 
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < n; j++) {

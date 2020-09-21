2b0ef632e6e5218bf17dd5060016cff2459a99f5
Alena Kropacheva <35008058+martilut@users.noreply.github.com>
2020-09-21 13:55:33 +0300

    Update 5max.c

--- 5max.c
+++ 5max.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-int main(){
-
+int main()
+{
     int n, i, j;
     long long int k;
     scanf("%d", &n);
@@ -9,18 +9,18 @@ int main(){
     int arr[n];
     int val[n];
 
-    for (i = 0; i < n; i++){
+    for (i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
         val[i] = 1;
-        for (j = 0; j < i; j++){
-            if (arr[j] == arr[i]){
+        for (j = 0; j < i; j++) {
+            if (arr[j] == arr[i]) {
                 val[i] = val[i] + 1;
                 val[j] = val[j] + 1;
             }
         }
     }
     k = -2147483649;
-    for (i = 0; i < n; i++){
+    for (i = 0; i < n; i++) {
         if (arr[i] > k && val[i] > 1)
             k = arr[i];
     }
@@ -28,5 +28,4 @@ int main(){
         printf("None");
     else
         printf("%d", k);
-
 }

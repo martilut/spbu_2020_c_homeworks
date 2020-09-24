40fdd74450c8319c372f2f44efcb94fce6d36a25
Alena <jeykolton@gmail.com>
2020-09-24 20:52:21 +0300

    1.3 updated

--- tasks/1.3substr.c
+++ tasks/1.3substr.c
@@ -2,28 +2,28 @@
 
 int main()
 {
-    int n = 0, m = 0;
+    int len_s1 = 0, len_s2 = 0;
     int i = 0, j = 0;
 
     printf("Enter string lenghts:\n");
-    scanf("%d %d", &n, &m);
+    scanf("%d %d", &len_s1, &len_s2);
 
     int counter = 0;
-    char s1[n];
-    char s2[m];
+    char s1[len_s1];
+    char s2[len_s2];
 
     printf("Enter the first string:\n");
     scanf("%s", &s1);
     printf("Enter the second string:\n");
     scanf("%s", &s2);
 
-    for (i = 0; i < n; i++) {
-        if (s1[i] == s2[0] && (n - i) >= m) {
+    for (i = 0; i < len_s1; i++) {
+        if (s1[i] == s2[0] && (len_s1 - i) >= len_s2) {
             j = 1;
-            while (s2[j] == s1[j + i] && (j + i) < n) {
+            while (s2[j] == s1[j + i] && (j + i) < len_s1) {
                 j = j + 1;
             }
-            if (j == m)
+            if (j == len_s2)
                 counter = counter + 1;
         }
     }

e6dc0770f5cbdc619c5f991fb8de2e293d46c451
Alena <jeykolton@gmail.com>
2020-09-29 22:04:21 +0300

    tasks edited

--- tasks_2/2.2_bulls_and_cows.c
+++ tasks_2/2.2_bulls_and_cows.c
@@ -21,8 +21,8 @@ int main()
     int attempt = 0;
     int n = 0, i = 0;
     int cows = 0, bulls = 0;
-    int values[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int number[] = {0, 0, 0, 0};
+    int values[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    int number[] = { 0, 0, 0, 0 };
 
     number[0] = rand() % 9 + 1;
     values[number[0]] = -1;

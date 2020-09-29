b70bd598e024392be40dba1e8df6c82bd78919b2
Alena <jeykolton@gmail.com>
2020-09-29 21:41:40 +0300

    tasks edited

--- library/commonUtils/numericOperations.c
+++ library/commonUtils/numericOperations.c
@@ -10,7 +10,7 @@ int max(int a, int b)
     return a > b ? a : b;
 }
 
-int compare(const void *i, const void *j)
+int compare(const int *i, const int *j)
 {
-  return *(int *)i - *(int *)j;
+  return *i - *j;
 }

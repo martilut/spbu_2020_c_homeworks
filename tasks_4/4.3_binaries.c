f19fe6d6ef6c88ca693c71d7ea010cb923fa7ab7
Alena <jeykolton@gmail.com>
2020-10-17 22:37:21 +0300

    task edited

--- tasks_4/4.3_binaries.c
+++ tasks_4/4.3_binaries.c
@@ -3,7 +3,7 @@
 
 void reverseAdditionalCode(int binaryArray[])
 {
-    for (int i = 0; i < 32; ++ i) {
+    for (int i = 0; i < 32; ++i) {
         binaryArray[i] = (binaryArray[i] == 1 ? 0 : 1);
     }
 }
